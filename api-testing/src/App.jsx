47ee056e0989cf5a7edbf7d06f9b645a90c28859
pankaj-2503 <pankajpj3103@gmail.com>
2024-02-05 22:03:53 +0530

    potd

---
+++ api-testing/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from '../components/Navbar'
+import Testimonial from '../components/Testimonial'
+import { Card, Footer } from 'flowbite-react'
+
+function App() {
+ 
+
+  return (
+    <div>
+       <Navbar/>
+       <Testimonial/>
+       
+       
+    </div>
+  )
+}
+
+export default App
